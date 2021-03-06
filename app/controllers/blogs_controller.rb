df45675d64d2aac5e93cceeb785f1aa49c23510c
chihiro-a <chihiro.i.0128@gmail.com>
2021-03-06 23:49:12 +0000

    [finish]3-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,8 +6,21 @@ class BlogsController < ApplicationController
   end
 
   def new
+    @blog = Blog.new
+  end
+
+  def create
+    blog = Blog.new(blog_params)
+    # モデル名はblogなのでblog_paramsにする
+    blog.save
+    redirect_to blogs_path
   end
 
   def edit
   end
+
+  def blog_params
+    params.require(:blog).permit(:title, :category, :body)
+  end
+
 end
