c56345ba0b7a2ab95802bda9cd42549b4f8e7958
chihiro-a <chihiro.i.0128@gmail.com>
2021-03-07 04:34:38 +0000

    [finish]3-7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -26,6 +26,14 @@ class BlogsController < ApplicationController
     blog = Blog.find(params[:id])
     blog.update(blog_params)
     redirect_to blog_path(blog)
+    # 投稿詳細へリダイレクト
+  end
+
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
+    # 投稿一覧へリダイレクト
   end
 
   def blog_params
